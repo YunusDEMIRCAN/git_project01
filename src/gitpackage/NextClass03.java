bacf88e1689d42c0b4dedcc3c547d3cdc6203219
YunusDemircan <yunusdemircan@gmail.com>
2020-03-14 16:20:44 -0400

    Albany 6 added

--- src/gitpackage/NextClass03.java
+++ src/gitpackage/NextClass03.java
@@ -3,7 +3,8 @@ package gitpackage;
 public class NextClass03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Merhabalar");
+		System.out.println("albany");
 
 	}
 
